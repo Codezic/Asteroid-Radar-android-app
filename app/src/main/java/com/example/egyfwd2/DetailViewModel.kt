c9300e7e4fc0e885e0147c3930d1c2d43d9b7cc8
faragzz <91439764+faragzz@users.noreply.github.com>
2022-12-25 22:45:45 +0200

    fix

--- app/src/main/java/com/example/egyfwd2/DetailViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.egyfwd2
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class DetailViewModel:ViewModel() {
-    lateinit var itemChoosed:AstroidHolder
-}
